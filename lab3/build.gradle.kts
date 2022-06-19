e06b24faf88f6be8a17335bfd6a926ed777b2105
PonyavinMV <96287277+PonyavinMV@users.noreply.github.com>
2022-06-19 13:44:56 +0300

    3

---
+++ lab3/build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+}
+
+tasks.getByName<Test>("test"){
+    useJUnitPlatform()
+}
